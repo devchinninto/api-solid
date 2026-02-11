3adc807625e381f45783583d8f8328ab167b72d4
Marcelle <marcellealves.dev@gmail.com>
2026-02-11 19:58:44 -0300

    feat: add prisma with postgresql

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
 import fastify from 'fastify'
+import { prisma } from './lib/prisma'
+
 export const app = fastify()
+
+prisma.user.create({
+  data: {
+    name: 'Marcelle',
+    email: 'marcelle@email.com'
+  }
+})
