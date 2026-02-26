b99496f93c78a75f0e7fc8be7cbb43bc1a7b9736
Marcelle <marcellealves.dev@gmail.com>
2026-02-26 17:17:38 -0300

    feat: implement PrismaCheckInsRepository with methods for gym operations

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,48 @@
+import { GymCreateInput } from 'prisma/generated/prisma/models'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+import { Gym } from 'prisma/generated/prisma/client'
+
+export class PrismaCheckInsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id
+      }
+    })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+          mode: 'insensitive'
+        }
+      },
+      take: 20,
+      skip: (page - 1) * 20
+    })
+
+    return gyms
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT * from gyms
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  async create(data: GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data
+    })
+
+    return gym
+  }
+}
