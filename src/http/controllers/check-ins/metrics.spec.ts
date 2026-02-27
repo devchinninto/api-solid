c58e2ab7df6a57f6a6241afb4e88d981014162f9
Marcelle <marcellealves.dev@gmail.com>
2026-02-27 15:16:06 -0300

    feat: add end-to-end tests for check-in metrics and validation functionality

--- src/http/controllers/check-ins/metrics.spec.ts
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -4,7 +4,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 import { prisma } from '@/lib/prisma'
 
-describe('Check-In History (e2e)', () => {
+describe('Check-in Metrics (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -13,7 +13,7 @@ describe('Check-In History (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to list history of check-ins', async () => {
+  it('should be able to get check-ins count', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
     const user = await prisma.user.findFirstOrThrow()
@@ -34,20 +34,11 @@ describe('Check-In History (e2e)', () => {
     })
 
     const response = await request(app.server)
-      .get(`/check-ins/history`)
+      .get(`/check-ins/metrics`)
       .set('Authorization', `Bearer ${token}`)
       .send()
 
     expect(response.statusCode).toEqual(200)
-    expect(response.body.checkIns).toEqual([
-      expect.objectContaining({
-        gymId: gym.id,
-        userId: user.id
-      }),
-      expect.objectContaining({
-        gymId: gym.id,
-        userId: user.id
-      })
-    ])
+    expect(response.body.checkInsCount).toEqual(2)
   })
 })
--- src/http/controllers/check-ins/validate.spec.ts
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -4,7 +4,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 import { prisma } from '@/lib/prisma'
 
-describe('Check-In History (e2e)', () => {
+describe('Validate Check-in (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -13,7 +13,7 @@ describe('Check-In History (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to list history of check-ins', async () => {
+  it('should be able to validate a check-in', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
     const user = await prisma.user.findFirstOrThrow()
@@ -26,28 +26,26 @@ describe('Check-In History (e2e)', () => {
       }
     })
 
-    await prisma.checkIn.createMany({
-      data: [
-        { gymId: gym.id, userId: user.id },
-        { gymId: gym.id, userId: user.id }
-      ]
+    let checkIn = await prisma.checkIn.create({
+      data: {
+        gymId: gym.id,
+        userId: user.id
+      }
     })
 
     const response = await request(app.server)
-      .get(`/check-ins/history`)
+      .patch(`/check-ins/${checkIn.id}/validate`)
       .set('Authorization', `Bearer ${token}`)
       .send()
 
-    expect(response.statusCode).toEqual(200)
-    expect(response.body.checkIns).toEqual([
-      expect.objectContaining({
-        gymId: gym.id,
-        userId: user.id
-      }),
-      expect.objectContaining({
-        gymId: gym.id,
-        userId: user.id
-      })
-    ])
+    expect(response.statusCode).toEqual(204)
+
+    checkIn = await prisma.checkIn.findUniqueOrThrow({
+      where: {
+        id: checkIn.id
+      }
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
   })
 })
