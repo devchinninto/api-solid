4f132b204481e53d627b489a6bd002e0fef22b20
Marcelle <marcellealves.dev@gmail.com>
2026-02-27 15:44:16 -0300

    feat: implement refresh token functionality with JWT and cookie handling

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,30 @@
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
-import fastifyJwt from '@fastify/jwt'
 import { usersRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m' // 10 minutes
+  }
+})
+
+app.register(fastifyCookie)
+
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkInsRoutes)
-
-app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
-})
 
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -31,9 +31,27 @@ export async function authenticate(
       }
     )
 
-    return reply.status(200).send({
-      token
-    })
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d'
+        }
+      }
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/', // The entire backend will have access
+        secure: true, // HTTPS
+        sameSite: true,
+        httpOnly: true
+      })
+      .status(200)
+      .send({
+        token
+      })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
